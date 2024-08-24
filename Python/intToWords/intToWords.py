7db62608bdf451a8642ebb2b16b494475fdc0efe
Andre Khmelnitsky <akhmelni@uci.edu>
2024-08-24 01:52:44 +0100

    finished this problem with the great god cache either landing on my side or really fucking me up, tbh it's kinda wild that my answer swings from 99% speed to 40% depending on cache hits

--- Python/intToWords/intToWords.py
+++ Python/intToWords/intToWords.py
@@ -1,85 +1,127 @@
 
 
+from functools import cache
+import gc
+
+
 class Solution:
     def numberToWords(self, num: int) -> str:
-        tmp_str = ""
+        return int_to_words(num)
 
+
+def int_to_words(num: int) -> str:
+    if num == 0:
+        return "Zero"
+
+    appending_words = ["", "Thousand", "Million", "Billion"]
+
+    ret_str = ''
+
+    for word in appending_words:
+        curr_eval = num % 1000
+        eval_str = handle_larger(curr_eval, word)
+        if ret_str == '':
+            ret_str = eval_str
+        elif eval_str != '':
+            ret_str = f'{eval_str} {ret_str}'
+
+        num //= 1000
         if num == 0:
-            return "Zero"
+            break
 
-        first_2_digits = num % 100
-        num //= 100
-
-        tmp_str += handle_below_100(first_2_digits)
-
-        tens = handle_tens(num)
-        tens += tmp_str
-        tmp_str = tens
-        tmp_str += handle_hundos(num)
-        tmp_str += handle_thousands(num)
-        tmp_str += handle_thousands(num)
-        tmp_str += handle_millis(num)
-        tmp_str += handle_billis(num)
-
-        return tmp_str
+    gc.collect()
+    return ret_str
 
 
-def numToWords(num: int) -> str:
-    tmp_str = ""
-
-    if 10 <= num % 100 < 20:
-        tmp_str += handle_teens(num)
-    else:
-        tmp_str += handle_ones(num)
-
-    return tmp_str
+def get_bottom_three_digits(num: int) -> int:
+    return num % 1000
 
 
-def handle_below_100(processed_num: int) -> str:
-    # processed num must be <= 99
-    if 10 <= processed_num % 100 < 20:
+@cache
+def handle_ones_no_zero(num: int) -> str:
+    _tens = {
+        0: '',
+        1: 'One',
+        2: 'Two',
+        3: 'Three',
+        4: 'Four',
+        5: 'Five',
+        6: 'Six',
+        7: 'Seven',
+        8: 'Eight',
+        9: 'Nine'
+    }
+    return _tens.get(num)
+
+
+def handle_teens(num: int) -> str:
+    # gc.collect()
+    match num:
+        case 10:
+            return "Ten"
+        case 11:
+            return "Eleven"
+        case 12:
+            return "Twelve"
+        case 13:
+            return "Thirteen"
+        case 14:
+            return "Fourteen"
+        case 15:
+            return "Fifteen"
+        case 16:
+            return "Sixteen"
+        case 17:
+            return "Seventeen"
+        case 18:
+            return "Eighteen"
+        case 19:
+            return "Nineteen"
+    _teens = {
+        10: 'Ten',
+        11: 'Eleven',
+        12: 'Twelve',
+        13: 'Thirteen',
+        14: 'Fourteen',
+        15: 'Fifteen',
+        16: 'Sixteen',
+        17: 'Seventeen',
+        18: 'Eighteen',
+        19: 'Nineteen',
+    }
+
+    return _teens.get(num)
+
+
+def handle_2_digits(processed_num: int) -> str:
+    if processed_num < 10:
+        return handle_ones_no_zero(processed_num)
+    elif processed_num < 20:
         return handle_teens(processed_num)
     else:
-        tens = handle_tens(processed_num)
-        if len(tens) > 0:
-            return tens + " " + handle_ones(processed_num)
-        else:
-            return handle_ones(processed_num % 10)
+        ones_processed_num = processed_num % 10
+        ones_str = handle_ones_no_zero(ones_processed_num)
+        processed_num //= 10
+        tens_str = handle_tens(processed_num)
+        return f'{tens_str} {ones_str}' if ones_str != '' else tens_str
 
 
-def handle_ones(num: int) -> str:
-    num %= 10
-    match num:
-        case 1:
-            return "One"
-        case 2:
-            return "Two"
-        case 3:
-            return "Three"
-        case 4:
-            return "Four"
-        case 5:
-            return "Five"
-        case 6:
-            return "Six"
-        case 7:
-            return "Seven"
-        case 8:
-            return "Eight"
-        case 9:
-            return "Nine"
-        case 0:
-            return ""
+def handle_3_digits(processed_num: int) -> str:
+    bottom_2 = processed_num % 100
+    bottom_res = handle_2_digits(bottom_2)
+    processed_num //= 100
+
+    hundreds_res = handle_hundreds(processed_num)
+    if hundreds_res == '':
+        return bottom_res
+
+    return f'{hundreds_res} {bottom_res}' if bottom_res != '' else hundreds_res
 
 
-def handle_tens(num: int) -> str:
+@cache
+def handle_tens(processed_num: int) -> str:
 
-    # num /= 100
-    num %= 100
-    # get last two digits of number, then remove the last one anyway
-    num //= 10
-
-    match num:
+    match processed_num:
         case 0:
             return ""
         case 1:
@@ -102,118 +144,15 @@ def handle_tens(num: int) -> str:
             return "Ninety"
 
 
-def handle_teens(num: int) -> str:
-    match num:
-        case 10:
-            return "Ten"
-        case 11:
-            return "Eleven"
-        case 12:
-            return "Twelve"
-        case 13:
-            return "Thirteen"
-        case 14:
-            return "Fourteen"
-        case 15:
-            return "Fifteen"
-        case 16:
-            return "Sixteen"
-        case 17:
-            return "Seventeen"
-        case 18:
-            return "Eighteen"
-        case 19:
-            return "Nineteen"
+def handle_hundreds(processed_num: int) -> str:
+    return f'{handle_ones_no_zero(processed_num)} Hundred' if handle_ones_no_zero(processed_num) != '' else ''
 
 
-def handle_hundos(processed_num: int) -> str:
-
-    if processed_num == 0:
-        return ""
-
-    # tmp: str = handle_ones(processed_num)
-    # tmp += " Hundred"
-    tmp: str = handle_ones(processed_num)
-    tmp += " Hundred"
-
-    print(f'Tmp = {tmp}')
-    if len(tmp) > 0:
-        return tmp
+def handle_larger(processed_num: int, appending_str: str) -> str:
+    hundreds_res = handle_3_digits(processed_num)
+    if appending_str == '':
+        return hundreds_res
+    elif hundreds_res == '':
+        return ''
     else:
-        return ""
-
-
-def handle_bases_larger_than_hundos(num: int, variable_base: int, appending_string: str) -> str:
-    if variable_base <= 3:
-        return ValueError
-    num %= 10**variable_base
-    num //= (10**(variable_base - 3))
-
-    if num == 0:
-        return ""
-
-    tmp: str = handle_hundos(num)
-    tmp += appending_string
-    return tmp
-
-
-def handle_thousands(num: int) -> str:
-    num %= 1_000_000
-    num //= 1_000
-
-    if num == 0:
-        return ""
-
-    tmp = handle_hundos(num)
-    tmp += " Thousand"
-
-
-def handle_millis(num: int) -> str:
-    num %= 1_000_000_000
-    num //= 1_000_000
-
-    if num == 0:
-        return ""
-
-    tmp = handle_hundos(num)
-    tmp += " Million"
-
-
-def handle_billis(num: int) -> str:
-    num %= 1_000_000_000
-    num //= 1_000_000
-
-    if num == 0:
-        return ""
-
-    tmp = handle_hundos(num)
-    tmp += " Billion"
-
-
-def handle_three_digits(processed_num: int) -> str:
-    bottom_two_digits = processed_num % 100
-    processed_num //= 100
-    processed_num %= 10
-    hundos = handle_hundos(processed_num)
-    if len(hundos) > 0:
-        return hundos + " " + handle_below_100(bottom_two_digits)
-    else:
-        return handle_below_100(bottom_two_digits)
-
-
-def handle_large_digits(processed_num: int, appending_string: str) -> str:
-    res = handle_hundos(int)
-
-    return res + " " + appending_string if len(res) > 0 else ""
-
-
-def handle_thousands(processed_num: int) -> str:
-    return handle_large_digits(processed_num, "Thousand")
-
-
-def handle_millions(processed_num: int) -> str:
-    return handle_large_digits(processed_num, "Million")
-
-
-def handle_billions(processed_num: int) -> str:
-    return handle_large_digits(processed_num, "Billion")
+        return f'{hundreds_res} {appending_str}'
--- Python/intToWords/intToWordsTest.py
+++ Python/intToWords/intToWordsTest.py
@@ -1,55 +1,97 @@
 from typing import List
 import intToWords
+import time
+import sys
 
 
-def test_teens(n1: int = 14, n1_ans: str = "Fourteen"):
-    print("Running test teens with", n1)
-    test_word = intToWords.handle_below_100(n1)
-    print("Test word result =\t", test_word)
-    assert test_word == n1_ans, True
+def test_wrapper(nums: List[int], nums_response: List[str], test_function):
+    print()
+    print(f'Running function: {test_function.__name__}')
+    print(f'inputs = {nums}')
+    print(f'expected results = {nums_response}')
+    failed_tests = list()
 
-    print("Passed test teens\n")
-
-
-def test_below_100(nums: List[int] = [14, 15, 28, 99, 1], nums_response: List[str] = ["Fourteen", "Fifteen", "Twenty Eight", "Ninety Nine", "One"]) -> None:
-    print("Running test below 100\n")
-
+    passed_test = True
     for num, num_response in zip(nums, nums_response):
-        print("Running test teens with", num)
-        test_word = intToWords.handle_below_100(num)
-        print("Test word result = ", test_word, "\texpected : ", num_response)
-        assert test_word == num_response, True
+        print(f'Running {test_function.__name__} with {num}: {num_response}')
+        result = test_function(num)
+        if result != num_response:
+            sys.stderr.write(f"Failed test: {num}, received |{
+                             result}| instead of |{num_response}| \n\n")
+            passed_test = False
+            failed_tests.append(num)
+        else:
+            print(f'successfully executed\n')
 
-        print("Passed test" + str(num) + '\n')
+    if passed_test:
+
+        print(f'SUCCESSFULLY EXECUTED {test_function.__name__}')
+    else:
+
+        sys.stderr.write(f'FAILED TEST: {test_function.__name__}\n\n')
+        time.sleep(1)
+        for test in failed_tests:
+            sys.stderr.write(f'Failed tests: {test}\n')
+
+    print()
 
 
-def test_hundreds(nums: List[int] = [1, 1, 2, 9, 1], nums_response: List[str] = ["One Hundred", "One Hundred", "Two Hundred", "Nine Hundred", "One Hundred"]) -> None:
-    print("Running test hundos\n")
-
-    for num, num_response in zip(nums, nums_response):
-        print("Running test hundreds with", num)
-        test_word = intToWords.handle_hundos(num)
-        print("Test word result = ", test_word, "\texpected : ", num_response)
-        assert test_word == num_response, True
-
-        print("Passed test" + str(num) + '\n')
+def ones_test(input: int) -> str:
+    return intToWords.handle_ones_no_zero(input)
 
 
-def test_three_digs(nums: List[int] = [123, 111, 21, 999, 1], nums_response: List[str] = ["One Hundred Twenty Three", "One Hundred Eleven", "Twenty One", "Nine Hundred Ninety Nine", "One"]) -> None:
-    print("Running test hundos\n")
+def hundreds_test(input: int) -> str:
+    return intToWords.handle_hundreds(input)
 
-    for num, num_response in zip(nums, nums_response):
-        print("Running test hundreds with", num)
-        test_word = intToWords.handle_three_digits(num)
-        print("Test word result = |", test_word,
-              "|\texpected : |", num_response)
-        assert test_word == num_response, True
 
-        print("Passed test " + str(num) + '\n')
+def below_hundred_test(input: int) -> str:
+    return intToWords.handle_2_digits(input)
+
+
+def three_dig_test(input: int) -> str:
+    return intToWords.handle_3_digits(input)
+
+
+def thousands_test(input: int) -> str:
+    return intToWords.handle_thousands(input)
+
+
+def generic_test(input: int) -> str:
+    return intToWords.handle_larger(input, '')
+
+
+def full_test(input: int) -> str:
+    return intToWords.int_to_words(input)
+
+# def three_dig_test_again
 
 
 if __name__ == '__main__':
     # test_teens()
     # test_below_100()
     # test_hundreds()
-    test_three_digs()
+    # test_wrapper(nums=[123, 111, 21, 999, 1, 0], nums_response=["One Hundred Twenty Three", "One Hundred Eleven",
+    #  "Twenty One", "Nine Hundred Ninety Nine", "One", "Zero"], test_function=test_three_digs)
+    # test_wrapper(nums=[0, 1, 2, 3, 4], nums_response=[
+    #              '', 'One', 'Two', 'Three', 'Four'], test_function=ones_test)
+
+    # test_wrapper(nums=[0, 10, 20, 30, 40, 21, 98, 14], nums_response=[
+    #              '', 'Ten', 'Twenty', 'Thirty', 'Forty', 'Twenty One', 'Ninety Eight', 'Fourteen'], test_function=below_hundred_test)
+
+    # test_wrapper(nums=[1, 2, 9], nums_response=[
+    #              "One Hundred", "Two Hundred", "Nine Hundred"], test_function=hundreds_test)
+
+    # test_wrapper(nums=[0, 1, 22, 200, 942], nums_response=[
+    #     "", "One", "Twenty Two", "Two Hundred", "Nine Hundred Forty Two"], test_function=three_dig_test)
+
+    # test_wrapper(nums=[0, 1, 22, 200, 942], nums_response=[
+    #     "", "One", "Twenty Two", "Two Hundred", "Nine Hundred Forty Two"], test_function=generic_test)
+
+    # test_wrapper(nums=[1, 124, 139], nums_response=[
+    #              "One Thousand", "One Hundred Twenty Four Thousand", "One Hundred Thirty Nine Thousand"], test_function=thousands_test)
+
+    # test_wrapper(nums)
+
+    test_wrapper(nums=[1, 124, 139, 200, 942, 1_111, 1_999_432, 2_000_000, 2_123_321_999, 1_000_010], nums_response=[
+                 'One', 'One Hundred Twenty Four', 'One Hundred Thirty Nine', "Two Hundred", "Nine Hundred Forty Two", "One Thousand One Hundred Eleven",
+                 "One Million Nine Hundred Ninety Nine Thousand Four Hundred Thirty Two", "Two Million", "Two Billion One Hundred Twenty Three Million Three Hundred Twenty One Thousand Nine Hundred Ninety Nine", "One Million Ten"], test_function=full_test)
